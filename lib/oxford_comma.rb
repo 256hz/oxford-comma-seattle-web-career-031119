cb76d7d979c499bc55ccf69ab50b7614cf65a08b
Abe D <iaryiou111@gmail.com>
2019-03-03 23:03:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   string = ""
   case 
   when array.count == 1
-    string += array
+    string += array[0]
   when array.count == 2
     string = array.join(" and ")
   when array.count > 2
