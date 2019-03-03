4901f16bfa02819acab8d624607d1c845680f29c
Abe D <iaryiou111@gmail.com>
2019-03-03 23:02:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,18 @@
 def oxford_comma(array)
   string = ""
   case 
-  when array.count = 1
+  when array.count == 1
     string += array
-  when array.count = 2
+  when array.count == 2
     string = array.join(" and ")
   when array.count > 2
     array.each.with_index(1) do |noun, index|
       case 
       when index < array.count - 2
         string += "#{noun}, "
-      when index = array.count - 1
+      when index == array.count - 1
         string += "#{noun} and "
-      when index = array.count
+      when index == array.count
         string += "#{noun}."
       end
     end
